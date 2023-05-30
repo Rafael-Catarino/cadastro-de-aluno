1d9d7864e71b9a4b6a3e399c91530c2ae891a8a9
rafael <rafaelscatarino@hotmail.com>
2023-05-30 09:33:47 -0300

    adicionando a class student

---
+++ student.py
@@ -0,0 +1,8 @@
+class Student:
+    def __init__(self, registration, name, note1, note2, note3, note4):
+        self.registration = registration
+        self.name = name
+        self.note1 = note1
+        self.note2 = note2
+        self.note3 = note3
+        self.note4 = note4
